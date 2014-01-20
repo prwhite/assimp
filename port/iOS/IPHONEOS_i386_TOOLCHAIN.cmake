c3036cb2342fdb87042b2193c1bc2726beddfda0
Payton White <payton@prehiti.net>
2014-01-19 23:15:20 -0800

    Generally fixed up the xcode project for current Xcode... puts build output in the right place, so this project can be added to other workspaces and everything will fall together correctly.  Also tried to fix the iOS cmake files, but those are still blowing up for me.
    Changes to cmake files are based on http://deltaorange.com/2013/01/17/adventures-in-importing-3d-assets/... they all look right, but things still looked for /Developer, which might be due to cmake caching some intermediate work.  Not particularly interesting to me as I prefer the project file.

--- port/iOS/IPHONEOS_i386_TOOLCHAIN.cmake
+++ port/iOS/IPHONEOS_i386_TOOLCHAIN.cmake
@@ -4,9 +4,9 @@ SET (CMAKE_CROSSCOMPILING   TRUE)
 SET (CMAKE_SYSTEM_NAME      "Darwin")
 SET (CMAKE_SYSTEM_PROCESSOR "i386")
 
-SET (SDKVER     "5.0")
-SET (DEVROOT    "/Developer/Platforms/iPhoneOS.platform/Developer")
-SET (SDKROOT    "/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS${SDKVER}.sdk")
+SET (SDKVER     "6.0")
+SET (DEVROOT    "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer")
+SET (SDKROOT    "${DEVROOT}/SDKs/iPhoneOS${SDKVER}.sdk")
 SET (CC         "${DEVROOT}/usr/bin/llvm-gcc")
 SET (CXX        "${DEVROOT}/usr/bin/llvm-g++")
 
--- port/iOS/build_ios.sh
+++ port/iOS/build_ios.sh
@@ -7,8 +7,8 @@
 
 BUILD_DIR="./lib/ios"
 
-IOS_BASE_SDK="5.0"
-IOS_DEPLOY_TGT="3.2"
+IOS_BASE_SDK="7.0"
+IOS_DEPLOY_TGT="6.0"
 
 setenv_all()
 {
@@ -29,37 +29,26 @@ setenv_all()
 	export CXXFLAGS=$CFLAGS
 }
 
-setenv_arm6()
-{
-	unset DEVROOT SDKROOT CFLAGS CC LD CPP CXX AR AS NM CXXCPP RANLIB LDFLAGS CPPFLAGS CXXFLAGS
-	export DEVROOT=/Developer/Platforms/iPhoneOS.platform/Developer
-	export SDKROOT=$DEVROOT/SDKs/iPhoneOS$IOS_BASE_SDK.sdk
-	export CFLAGS="-arch armv6 -pipe -no-cpp-precomp -isysroot $SDKROOT -miphoneos-version-min=$IOS_DEPLOY_TGT -I$SDKROOT/usr/include/"
-	setenv_all
-	rm CMakeCache.txt
-	cmake  -G 'Unix Makefiles' -DCMAKE_TOOLCHAIN_FILE=./port/iOS/IPHONEOS_ARM6_TOOLCHAIN.cmake -DENABLE_BOOST_WORKAROUND=ON -DBUILD_STATIC_LIB=ON
-}
-
 setenv_arm7()
 {
 	unset DEVROOT SDKROOT CFLAGS CC LD CPP CXX AR AS NM CXXCPP RANLIB LDFLAGS CPPFLAGS CXXFLAGS
-	export DEVROOT=/Developer/Platforms/iPhoneOS.platform/Developer
+	export DEVROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer
 	export SDKROOT=$DEVROOT/SDKs/iPhoneOS$IOS_BASE_SDK.sdk
 	export CFLAGS="-arch armv7 -pipe -no-cpp-precomp -isysroot $SDKROOT -miphoneos-version-min=$IOS_DEPLOY_TGT -I$SDKROOT/usr/include/"
 	setenv_all
 	rm CMakeCache.txt
-	cmake  -G 'Unix Makefiles' -DCMAKE_TOOLCHAIN_FILE=./port/iOS/IPHONEOS_ARM7_TOOLCHAIN.cmake -DENABLE_BOOST_WORKAROUND=ON -DBUILD_STATIC_LIB=ON
+	cmake  -G 'Unix Makefiles' -DCMAKE_TOOLCHAIN_FILE=./port/iOS/IPHONEOS_ARM7_TOOLCHAIN.cmake -DASSIMP_ENABLE_BOOST_WORKAROUND=ON -DASSIMP_BUILD_STATIC_LIB=ON
 }
 
 setenv_i386()
 {
 	unset DEVROOT SDKROOT CFLAGS CC LD CPP CXX AR AS NM CXXCPP RANLIB LDFLAGS CPPFLAGS CXXFLAGS
-	export DEVROOT=/Developer/Platforms/iPhoneSimulator.platform/Developer
+	export DEVROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer
 	export SDKROOT=$DEVROOT/SDKs/iPhoneSimulator$IOS_BASE_SDK.sdk
 	export CFLAGS="-arch i386 -pipe -no-cpp-precomp -isysroot $SDKROOT -miphoneos-version-min=$IOS_DEPLOY_TGT"
 	setenv_all
 	rm CMakeCache.txt
-	cmake  -G 'Unix Makefiles' -DCMAKE_TOOLCHAIN_FILE=./port/iOS/IPHONEOS_i386_TOOLCHAIN.cmake -DENABLE_BOOST_WORKAROUND=ON -DBUILD_STATIC_LIB=ON
+	cmake  -G 'Unix Makefiles' -DCMAKE_TOOLCHAIN_FILE=./port/iOS/IPHONEOS_i386_TOOLCHAIN.cmake -DASSIMP_ENABLE_BOOST_WORKAROUND=ON -DASSIMP_BUILD_STATIC_LIB=ON
 }
 
 create_outdir()
@@ -69,20 +58,14 @@ create_outdir()
 		lib_arm7=`echo $lib_i386 | sed "s/i386/arm7/g"`
 		lib=`echo $lib_i386 | sed "s/i386\///g"`
 		echo 'Creating fat binary...'
-		lipo -arch armv6 $lib_arm6 -arch armv7 $lib_arm7 -arch i386 $lib_i386 -create -output $lib
+		lipo -arch -arch armv7 $lib_arm7 -arch i386 $lib_i386 -create -output $lib
 	done
 	echo 'Done! You will find the libaries and fat binary library in /lib/ios'
 }
 cd ../../
 
 rm -rf $BUILD_DIR
-mkdir -p $BUILD_DIR/arm6 $BUILD_DIR/arm7 $BUILD_DIR/i386
-
-setenv_arm6
-echo 'Building armv6 library'
-make clean
-make assimp -j 8 -l
-cp ./lib/libassimp.a $BUILD_DIR/arm6/
+mkdir -p $BUILD_DIR/arm7 $BUILD_DIR/i386
 
 setenv_arm7
 echo 'Building armv7 library'
